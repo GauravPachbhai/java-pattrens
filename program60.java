5a8235bd10b04a406ca6abfd8328e6cb95e38a86
Gaurav Pachbhai <104085687+GauravPachbhai@users.noreply.github.com>
2023-09-13 21:39:54 +0530

    Add files via upload

---
+++ program60.java
@@ -0,0 +1,24 @@
+/*
+abcde
+ bcde
+  cde
+   de
+    e
+*/
+
+class program60
+{
+	public static void main(String args[]){
+		int n=5;
+		for(int i=1; i<=n;i++){
+			for(int j=i;j>1;j--){
+				System.out.print(" ");
+			}
+			for(int j=i; j<=n;j++){
+				System.out.print((char)(j+96));
+			}
+			
+			System.out.println();
+		}
+	}
+}
